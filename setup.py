ea03d7b66761fd149ef60e48883f78487f08016f
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-11-18 09:38:43 +1300

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/GNS-Science/nshm-toshi-client'
 EMAIL = 'chrisbc@artisan.co.nz'
 AUTHOR = 'GNS NSHM programme'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
