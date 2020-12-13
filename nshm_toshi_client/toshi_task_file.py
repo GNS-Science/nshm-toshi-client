a9a652587b97cc26740bba413b2de8545302c332
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-12-14 12:57:58 +1300

    fix for API file_link modification

--- nshm_toshi_client/toshi_task_file.py
+++ nshm_toshi_client/toshi_task_file.py
@@ -5,22 +5,22 @@ class ToshiTaskFile(ToshiClientBase):
     def __init__(self, url, auth_token, with_schema_validation=True, headers=None ):
         super(ToshiTaskFile, self).__init__(url, auth_token, with_schema_validation, headers)
 
-    def create_task_file(self, task_id, file_id, task_role):
+    def create_task_file(self, task_id, file_id, role):
         qry = '''
-        mutation create_task_file(
+        mutation create_file_link(
             $task_id:ID!
             $file_id:ID!
-            $task_role:TaskFileRole!) {
-              create_task_file(
+            $role:FileRole!) {
+              create_file_link(
                 file_id:$file_id
-                task_id:$task_id
-                task_role:$task_role
+                thing_id:$task_id
+                role:$role
               )
             {
-              task_file { id }
+              file_link { id }
             }
         }'''
-
-        variables = dict(task_id=task_id, file_id=file_id, task_role=task_role)
+        variables = dict(task_id=task_id, file_id=file_id, role=task_role)
         executed = self.run_query(qry, variables)
-        return executed['create_task_file']['task_file']['id']
+        return executed['create_file_link']['file_link']['id']
+
