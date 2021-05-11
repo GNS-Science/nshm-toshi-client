5c6d31ea88597fa7c24d9666854c5d4abd90ed09
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-05-12 09:33:13 +1200

    fix args; bump;

--- nshm_toshi_client/general_task.py
+++ nshm_toshi_client/general_task.py
@@ -17,22 +17,22 @@ class GeneralTask(ToshiClientBase):
         self.file_api = ToshiFile(toshi_api_url, s3_url, auth_token, with_schema_validation, headers)
         self.task_file_api = ToshiTaskFile(toshi_api_url, auth_token, with_schema_validation, headers)
 
-    def upload_file(self, filepath):
-        filepath = PurePath(filepath)
-        file_id, post_url = self.file_api.create_file(filepath)
-        self.file_api.upload_content(post_url, filepath)
-        return file_id
+    # def upload_file(self, filepath):
+    #     filepath = PurePath(filepath)
+    #     file_id, post_url = self.file_api.create_file(filepath)
+    #     self.file_api.upload_content(post_url, filepath)
+    #     return file_id
 
-    def link_task_file(self, task_id, file_id, task_role):
-        return self.task_file_api.create_task_file(task_id, file_id, task_role)
+    # def link_task_file(self, task_id, file_id, task_role):
+    #     return self.task_file_api.create_task_file(task_id, file_id, task_role)
 
-    def upload_task_file(self, task_id, filepath, task_role):
-        filepath = PurePath(filepath)
-        file_id = self.upload_file(filepath)
-        #link file to task in role
-        return self.link_task_file(task_id, file_id, task_role)
+    # def upload_task_file(self, task_id, filepath, task_role):
+    #     filepath = PurePath(filepath)
+    #     file_id = self.upload_file(filepath)
+    #     #link file to task in role
+    #     return self.link_task_file(task_id, file_id, task_role)
 
-    def create_task(self, input_variables, arguments=None, environment=None):
+    def create_task(self, created, agent_name, title, description):
         '''
         created: DateTime
         When the taskrecord was created
@@ -63,7 +63,7 @@ class GeneralTask(ToshiClientBase):
             }
         '''
         print(qry)
-        self.validate_variables(self.get_example_create_variables(), input_variables)
+        input_variables = dict(created=created, agent_name=agent_name, title=title, description=description)
         executed = self.run_query(qry, input_variables)
         return executed['create_general_task']['task_result']['id']
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/GNS-Science/nshm-toshi-client'
 EMAIL = 'chrisbc@artisan.co.nz'
 AUTHOR = 'GNS NSHM programme'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.8'
+VERSION = '0.2.9'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
