a4796db3110ae789b7498dcd0d8a27f801a60398
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-11-10 16:32:34 +1300

    Bump version: 1.0.0 → 1.0.1

--- nshm_toshi_client/__init__.py
+++ nshm_toshi_client/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Chris B Chamberlain"""
 __email__ = 'chrisbc@artisan.co.nz'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from .config import API_KEY, API_URL, S3_URL
 from .toshi_file import ToshiFile
