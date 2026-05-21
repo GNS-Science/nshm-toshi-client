e25949e265137b5b708c4e5c5c79b696c55caaf1
chrisdicaprio <christopher.dicaprio@gmail.com>
2026-05-22 07:52:21 +1200

    hoist commonly used constants and functions

--- nshm_toshi_client/__init__.py
+++ nshm_toshi_client/__init__.py
@@ -3,5 +3,7 @@
 __author__ = """Chris B Chamberlain"""
 __email__ = 'chrisbc@artisan.co.nz'
 from ._version import __version__
-from .config import API_KEY, API_URL, S3_URL
+from .config import API_KEY, API_URL, S3_URL, get_auth_kwargs
+from .general_task import GeneralTask
+from .toshi_client_base import ToshiClientBase
 from .toshi_file import ToshiFile
