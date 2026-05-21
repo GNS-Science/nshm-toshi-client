2d97b4f999bc535825506a2de4efe65209a888ca
chrisdicaprio <christopher.dicaprio@gmail.com>
2026-05-22 07:55:27 +1200

    hoist more constants

--- nshm_toshi_client/__init__.py
+++ nshm_toshi_client/__init__.py
@@ -3,7 +3,17 @@
 __author__ = """Chris B Chamberlain"""
 __email__ = 'chrisbc@artisan.co.nz'
 from ._version import __version__
-from .config import API_KEY, API_URL, S3_URL, get_auth_kwargs
+from .config import (
+    API_KEY,
+    API_URL,
+    COGNITO_DOMAIN,
+    COGNITO_REGION,
+    COGNITO_SCIENTIST_CLIENT_ID,
+    COGNITO_USER_POOL_ID,
+    M2M_SECRET_ARN,
+    S3_URL,
+    get_auth_kwargs,
+)
 from .general_task import GeneralTask
 from .toshi_client_base import ToshiClientBase
 from .toshi_file import ToshiFile
